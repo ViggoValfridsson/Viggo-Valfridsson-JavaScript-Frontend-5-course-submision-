7c187abf3d51e7c3bd00411219b2d371f5a6929b
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2022-11-11 12:55:05 +0100

    Added test page that shows test results

--- src/js/test/mainTest.js
+++ src/js/test/mainTest.js
@@ -2,7 +2,7 @@ const assert = require("chai").assert;
 const main = require("../main");
 
 describe("addDateToObjects", () => {
-  it("Takes object from an array, checks first_brewed property, reads the date string in YYYY or MM/YYYY format, converts it to date object and adds it.", () => {
+  it("Takes objects from an array, checks first_brewed property, reads the date string in YYYY or MM/YYYY format, converts it to date object and adds it.", () => {
     let testArray = [{ first_brewed: "02/2022" }, { first_brewed: "2022" }];
     main.addDateToObjects(testArray);
     assert.equal(testArray[0].date.getFullYear(), 2022);
